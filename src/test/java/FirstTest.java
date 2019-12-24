4dcaace7103ac5fa1fefb533302558f324e64017
meir-bar-shay <58541781+meir-bar-shay@users.noreply.github.com>
2019-12-24 13:06:56 +0200

    Update Classwork_Meir

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,34 +1,24 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class FirstTest {
-    WebDriver driver;
-    @BeforeMethod
-    public void setUp(){
-        driver = new ChromeDriver();
-    }
+public class FirstTest extends TestBase {
+
     @Test
-    public void openChromeTest(){
+    public void openChromeTest() throws InterruptedException {
         driver.get("https://www.tel-ran.com/");
-}
+        Thread.sleep(5000);
+    }
+
+
     @Test
     public void openWikipediaTest() throws InterruptedException {
         driver.get("https://www.wikipedia.org/");
-        Thread.sleep(5000);
+        pause(5000);
 
         driver.findElement(By.id("searchInput")).click();
         driver.findElement(By.id("searchInput")).clear();
         driver.findElement(By.id("searchInput")).sendKeys("Java");
-
         driver.findElement(By.className("svg-search-icon")).click();
-    }
-
-    @AfterMethod
-    public void tearDown(){
-        driver.quit();
+        pause(5000);
     }
 }
